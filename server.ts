92aa4c215b350922acccf6902961148a0505e11f
taisan <131413638+taisan11@users.noreply.github.com>
2023-11-06 00:30:02 +0000

    成功たのむ

--- server.ts
+++ server.ts
@@ -3,26 +3,26 @@ import { Hono } from "hono";
 const app: Hono = new Hono();
 
 // app.get('/', (c) => c.redirect("https://github.com/taisan11/linebot-Tnet"))
-app.post("/api/webhook/", async (c) => {
+app.post("/api/webhook/", (c) => {
   console.log(JSON.stringify(c));
-  const replys: Promise<Response>[] = []
+  const replys: Promise<Response>[] = [];
   for (const event of data.events) {
     // イベントでループ
-    if (event.type !== 'message') return // メッセージでないイベントは無視
+    if (event.type !== "message") return; // メッセージでないイベントは無視
 
-    const { message, replyToken } = event
+    const { message, replyToken } = event;
 
-    if (message.type !== 'text') return // テキストメッセージでないイベントは無視
+    if (message.type !== "text") return; // テキストメッセージでないイベントは無視
 
-    const textMessage: string = message.text // ユーザーの発言を取得
+    const textMessage: string = message.text; // ユーザーの発言を取得
 
     const replyData = {
       replyToken,
       messages: [{
         type: "text",
-        text: `${textMessage}`
+        text: `${textMessage}`,
       }],
-    } // リプライするデータを作成
+    }; // リプライするデータを作成
     replys.push(fetch("https://api.line.me/v2/bot/message/reply", {
       method: "POST",
       headers: {
@@ -30,13 +30,12 @@ app.post("/api/webhook/", async (c) => {
         "Authorization": "Bearer " + Deno.env.get("LINE_TOKEN"),
       },
       "body": JSON.stringify(replyData),
-    })) // リプライ
+    })); // リプライ
   }
-  await Promise.all(replys) // 全てのリプライ完了を待つ
   return c.jsonT({ message: "Hello World!" });
 });
 
-app.notFound((c) => {
+app.get('*', (c) =>{
   return c.text("NotFound");
 });
 
