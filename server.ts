729905aa2bcab190077bfb6dce1439455f73811b
taisan <131413638+taisan11@users.noreply.github.com>
2023-11-06 00:21:12 +0000

    add

--- server.ts
+++ server.ts
@@ -3,8 +3,36 @@ import { Hono } from "hono";
 const app: Hono = new Hono();
 
 // app.get('/', (c) => c.redirect("https://github.com/taisan11/linebot-Tnet"))
-app.post("/api/webhook", (c) => {
+app.post("/api/webhook", async (c) => {
   console.log(JSON.stringify(c));
+  const replys: Promise<Response>[] = []
+  for (const event of data.events) {
+    // イベントでループ
+    if (event.type !== 'message') return // メッセージでないイベントは無視
+
+    const { message, replyToken } = event
+
+    if (message.type !== 'text') return // テキストメッセージでないイベントは無視
+
+    const textMessage: string = message.text // ユーザーの発言を取得
+
+    const replyData = {
+      replyToken,
+      messages: [{
+        type: "text",
+        text: `${textMessage}`
+      }],
+    } // リプライするデータを作成
+    replys.push(fetch("https://api.line.me/v2/bot/message/reply", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        "Authorization": "Bearer " + Deno.env.get("LINE_TOKEN"),
+      },
+      "body": JSON.stringify(replyData),
+    })) // リプライ
+  }
+  await Promise.all(replys) // 全てのリプライ完了を待つ
   return c.jsonT({ message: "Hello World!" });
 });
 
